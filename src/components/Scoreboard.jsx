6a98b2b0f571c11e1c41e01fa839fb42e8fd618a
norkitorki <norki@tuta.io>
2025-04-12 13:54:20 +0200

    Create Scoreboard component

---
+++ src/components/Scoreboard.jsx
@@ -0,0 +1,20 @@
+import '../style/Scoreboard.css';
+
+export default function Scoreboard({ score, maxScore, maxPossible }) {
+  return (
+    <table className="gameScore">
+      <tbody>
+        <tr>
+          <th>Current Score</th>
+          <td>{score}</td>
+        </tr>
+        <tr>
+          <th>Max Score</th>
+          <td>
+            {maxScore} {maxPossible && `(Max possible: ${maxPossible})`}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
