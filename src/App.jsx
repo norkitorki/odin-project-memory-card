15cda08d0c58e3f7d82ff1855c5bb696f4dbae2d
norkitorki <norki@tuta.io>
2025-04-12 14:00:02 +0200

    Create App component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './style/App.css';
+import MemoryGame from './components/MemoryGame';
+
+export default function App() {
+  return (
+    <>
+      <h1>EU Member States Memory Card</h1>
+      <MemoryGame />
+    </>
+  );
+}
