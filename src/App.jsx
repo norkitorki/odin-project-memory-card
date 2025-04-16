0d3939d1ce69c3d95129eb2e94848bc9b59f2e45
norkitorki <norki@tuta.io>
2025-04-16 11:07:41 +0200

    Define header section

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ export default function App() {
 
   return (
     <>
-      <h1>EU Members Memory Card</h1>
+      <header>
+        <h1>EU Members Memory Card</h1>
+        <p>
+          Can you remember every member state of the European Union? Click on
+          every country exactly <b>once</b> to win the game!
+        </p>
+      </header>
       <MemoryGame images={countryFlags} />
     </>
   );
