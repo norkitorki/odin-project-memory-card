f629c3e9ec422e6e0750f23bdf6795a4117dada2
norkitorki <norki@tuta.io>
2025-04-16 07:19:16 +0200

    Handle winning condition

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -8,12 +8,19 @@ export default function MemoryGame({ images }) {
   const [maxScore, setMaxScore] = useState(0);
   const selection = useRef({});
 
+  const maxPossible = images.length;
   const shuffledImages = images.toSorted(() => Math.random() - 0.5);
 
   const handleClick = (id) => {
     if (selection.current[id]) {
       selection.current = {};
       return setScore(0);
+    }
+
+    if (score + 1 >= maxPossible) {
+      alert('Congratulations! You have won the game.');
+      setScore(0);
+      return setMaxScore(0);
     }
 
     selection.current[id] = 1;
@@ -23,7 +30,7 @@ export default function MemoryGame({ images }) {
 
   return (
     <>
-      <Scoreboard score={score} maxScore={maxScore} maxPossible={27} />
+      <Scoreboard score={score} maxScore={maxScore} maxPossible={maxPossible} />
       <div className="cardContainer">
         {shuffledImages.length < 1
           ? 'Images are loading...'
