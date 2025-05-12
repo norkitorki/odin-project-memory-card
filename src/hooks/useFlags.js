4cd39879db1772beacc2623c3084fc836b16c6a4
norkitorki <norki@tuta.io>
2025-05-12 11:08:48 +0200

    Export countryData into its own file

--- src/hooks/useFlags.js
+++ src/hooks/useFlags.js
@@ -1,34 +1,5 @@
 import { useState, useEffect } from 'react';
-
-const countryData = [
-  { id: 'at', text: 'Austria' },
-  { id: 'be', text: 'Belgium' },
-  { id: 'bg', text: 'Bulgaria' },
-  { id: 'hr', text: 'Croatia' },
-  { id: 'cy', text: 'Cyprus' },
-  { id: 'cz', text: 'Czechia' },
-  { id: 'dk', text: 'Denmark' },
-  { id: 'ee', text: 'Estonia' },
-  { id: 'fi', text: 'Finland' },
-  { id: 'fr', text: 'France' },
-  { id: 'de', text: 'Germany' },
-  { id: 'gr', text: 'Greece' },
-  { id: 'hu', text: 'Hungary' },
-  { id: 'ie', text: 'Ireland' },
-  { id: 'it', text: 'Italy' },
-  { id: 'lv', text: 'Latvia' },
-  { id: 'lt', text: 'Lithuania' },
-  { id: 'lu', text: 'Luxembourg' },
-  { id: 'mt', text: 'Malta' },
-  { id: 'nl', text: 'Netherlands' },
-  { id: 'pl', text: 'Poland' },
-  { id: 'pt', text: 'Portugal' },
-  { id: 'ro', text: 'Romania' },
-  { id: 'sk', text: 'Slovakia' },
-  { id: 'si', text: 'Slovenia' },
-  { id: 'es', text: 'Spain' },
-  { id: 'se', text: 'Sweden' },
-];
+import countryData from '../assets/static/countryData';
 
 export default function useFlags() {
   const [flags, setFlags] = useState([]);
